bd2c6dc9ca7e943bd1c85d8c94a7afda01e74e3e
wangym <wangym0203@gmail.com>
2024-12-21 07:33:00 +0800

    update

--- lib/config/env_config.dart
+++ lib/config/env_config.dart
@@ -8,21 +8,13 @@ class EnvConfig {
   EnvConfig._internal();
 
   late Environment environment;
-  late String apiBaseUrl;
 
   Future<void> initialize(Environment env) async {
     environment = env;
     // 根据环境加载不同的 .env 文件
     await dotenv.load(fileName: env == Environment.dev ? '.env.dev' : '.env.prod');
-
-    switch (environment) {
-      case Environment.dev:
-        apiBaseUrl = 'http://localhost:4000';
-
-      case Environment.prod:
-        apiBaseUrl = 'https://api.yyuu.cyou/';
-    }
   }
 
   String get debugToken => dotenv.env['DEBUG_TOKEN'] ?? "";
+  String get apiBaseUrl => dotenv.env['apiBaseUrl'] ?? "";
 }
